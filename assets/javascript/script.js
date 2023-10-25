d6225407b27b2bfb168b36ad559ecd93a0fae843
Clayton Kennedy <clayton-kennedy@hotmail.com>
2023-10-25 19:04:53 -0300

    First Commit

---
+++ assets/javascript/script.js
@@ -0,0 +1,21 @@
+
+// const cardProject1 = document.querySelector('#cardProject1');
+// const btnVisualizar = document.querySelector('#btnVisualizar');
+// const btnRepositorio = document.querySelector('#btnRepositorio');
+
+// cardProject1.addEventListener('mouseover', () => {
+//      cardProject1.style.backgroundColor = '#0000006b';
+//      cardProject1.style.backgroundSize ='120%';
+//      cardProject1.style.transition = '0.6s';
+//      cardProject1.style.filter = 'none';
+//      btnVisualizar.style.display = 'flex';
+//      btnRepositorio.style.display = 'flex';
+// });
+
+// cardProject1.addEventListener('mouseout', () => {
+//      cardProject1.style.backgroundSize ='10%';
+//      cardProject1.style.transition = '0.3s';
+//      cardProject1.style.filter = 'grayscale(100%)';
+//      btnVisualizar.style.display = 'none';
+//      btnRepositorio.style.display = 'none';
+// });
